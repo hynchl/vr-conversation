e57048a81547e35fcbe5101adea39ff083463f52
Hyunchul Kim <30741095+hynchl@users.noreply.github.com>
2024-04-02 16:49:33 +0900

    add controls

--- Assets/SetRemotePosition.cs
+++ Assets/SetRemotePosition.cs
@@ -6,7 +6,7 @@ public class SetRemotePosition : MonoBehaviour
 {
     void Start()
     {
-        transform.position = GameObject.Find("RemotePosition").transform.position;
-        transform.rotation = GameObject.Find("RemotePosition").transform.rotation;
+        // transform.position = GameObject.Find("RemotePosition").transform.position;
+        // transform.rotation = GameObject.Find("RemotePosition").transform.rotation;
     }
 }
