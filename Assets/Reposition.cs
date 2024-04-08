25f3f640e7b352f52ea3957500e6f53993cb2a66
Hyunchul Kim <30741095+hynchl@users.noreply.github.com>
2024-04-08 21:50:05 +0900

    fix

--- Assets/Reposition.cs
+++ Assets/Reposition.cs
@@ -11,7 +11,7 @@ public class Reposition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        DoReposition();
     }
 
     // Update is called once per frame
@@ -19,17 +19,20 @@ public class Reposition : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            // ...
-            // OVR Anchor 의 포지션이 타겟 포지션 사이의 차이를 측정해서
-            // 그만큼 OVRCameraRig과 local avatar들을 옮겨라
-            Vector3 diff = target.position - current.position;
-            foreach (Transform tf in itemsToMove)
-            {
-                tf.position = tf.position + diff;
-                Vector3 eulerAngle = tf.eulerAngles;
-                eulerAngle.y = target.eulerAngles.y;
-                tf.eulerAngles = eulerAngle;
-            }
+            DoReposition();
         }
     }
+
+    void DoReposition()
+    {
+        Vector3 diff = target.position - current.position;
+        foreach (Transform tf in itemsToMove)
+        {
+            tf.position = tf.position + diff;
+            Vector3 eulerAngle = tf.eulerAngles;
+            eulerAngle.y = target.eulerAngles.y;
+            tf.eulerAngles = eulerAngle;
+        }
+    }
+
 }
